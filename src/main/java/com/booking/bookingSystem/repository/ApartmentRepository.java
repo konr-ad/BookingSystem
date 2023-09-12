a105022f34d691d8d7b367357046420756dd944f
Konrad <konradk95@gmail.com>
2023-09-12 20:35:31 +0200

    fix repositories

--- src/main/java/com/booking/bookingSystem/repository/ApartmentRepository.java
+++ src/main/java/com/booking/bookingSystem/repository/ApartmentRepository.java
@@ -3,11 +3,13 @@ package com.booking.bookingSystem.repository;
 import com.booking.bookingSystem.model.Apartment;
 import com.booking.bookingSystem.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
 
     Optional<Apartment>findById(Long id);
--- src/main/java/com/booking/bookingSystem/repository/ReservationRepository.java
+++ src/main/java/com/booking/bookingSystem/repository/ReservationRepository.java
@@ -4,10 +4,12 @@ import com.booking.bookingSystem.model.Client;
 import com.booking.bookingSystem.model.Apartment;
 import com.booking.bookingSystem.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findByClient(Client client);
