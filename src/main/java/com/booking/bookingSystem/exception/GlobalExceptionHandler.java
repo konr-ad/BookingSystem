349d72fea317c8637ee452c253b772b9395a153b
Konrad <konradk95@gmail.com>
2023-11-18 15:34:17 +0100

    Adding GlobalExceptionHandler

---
+++ src/main/java/com/booking/bookingSystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.booking.bookingSystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException enfe) {
+        return new ResponseEntity<>(enfe.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
